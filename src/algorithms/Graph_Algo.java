1f28f6efebf3618480d5bc1f0bf03f4a13128c35
OrianHindi <orianhindi44@gmail.com>
2019-12-18 18:27:25 +0200

    src files

---
+++ src/algorithms/Graph_Algo.java
@@ -0,0 +1,63 @@
+package algorithms;
+
+import java.util.List;
+
+import dataStructure.graph;
+import dataStructure.node_data;
+/**
+ * This empty class represents the set of graph-theory algorithms
+ * which should be implemented as part of Ex2 - Do edit this class.
+ * @author 
+ *
+ */
+public class Graph_Algo implements graph_algorithms{
+
+	@Override
+	public void init(graph g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(String file_name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void save(String file_name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isConnected() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double shortestPathDist(int src, int dest) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<node_data> shortestPath(int src, int dest) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<node_data> TSP(List<Integer> targets) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public graph copy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/dataStructure/DGraph.java
@@ -0,0 +1,73 @@
+package dataStructure;
+
+import java.util.Collection;
+
+public class DGraph implements graph{
+
+	@Override
+	public node_data getNode(int key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public edge_data getEdge(int src, int dest) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addNode(node_data n) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void connect(int src, int dest, double w) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Collection<node_data> getV() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<edge_data> getE(int node_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public node_data removeNode(int key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public edge_data removeEdge(int src, int dest) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int nodeSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int edgeSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMC() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
